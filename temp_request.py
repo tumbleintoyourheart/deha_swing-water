7c8f8df9b389a69603a71e64d2bcafd8f2c8a944
tumbleintoyourheart <doubleliftnv@gmail.com>
2020-03-19 12:22:39 +0700

    .

---
+++ temp_request.py
@@ -0,0 +1,10 @@
+import pickle
+import numpy as np
+import renom as rm
+import pandas as pd
+import requests
+from sklearn import preprocessing
+from renom_rg.api.interface.regressor import Regressor
+
+url = '0.0.0.0:80'
+requests.post(url)
