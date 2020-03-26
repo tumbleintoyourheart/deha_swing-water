be8cbafe9019029de7071d8f110381315d3ad1af
tumbleintoyourheart <doubleliftnv@gmail.com>
2020-03-26 16:13:23 +0700

    add test_request

--- test_request.py
+++ test_request.py
@@ -3,7 +3,6 @@ import requests
 
 
 url = 'http://0.0.0.0:5000/upload_model'
-files = {'scaler_x': open('./module1_sklearn/models/200310_atg_dsp_sk_rf_nos.pickle', 'rb'),
-         'scaler_y': open('./module1_sklearn/models/200310_atg_dsp_sk_rf_std.pickle', 'rb')}
+files = {'models': open('./module1_sklearn/models/200310_atg_dsp_sk_rf_nos.pickle', 'rb')}
 res = requests.post(url, files=files)
 print(res.text)
