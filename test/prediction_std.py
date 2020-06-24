bdf5242ad5a212f40c8b6c2fa71621637e505607
Hieu NT2 <hieu.nt@deha-soft.com>
2020-06-24 10:25:35 +0700

    bug test

---
+++ test/prediction_std.py
@@ -0,0 +1,34 @@
+import pickle
+import numpy as np
+import renom as rm
+import pandas as pd
+import requests
+from sklearn import preprocessing
+from renom_rg.api.interface.regressor import Regressor
+
+HOST = "13.230.125.129"
+PORT = "80"
+url = "http://{}".format(HOST)
+model_id = '7'
+deploy_api = url + ':' + PORT + '/api/renom_rg/models/' + model_id + '/deploy'
+requests.post(deploy_api)
+regressor = Regressor(url, PORT)
+regressor.pull()
+
+pred_data = pd.read_csv("prediction.csv")
+
+setsumei_list = list(pred_data.columns)
+y_col = pd.DataFrame(data, columns=['mokuteki'])
+x_col = pd.DataFrame(data, columns=setsumei_list)
+
+with open("./scaler_x.pickle", mode='rb') as f:
+    scaler_X_standardization = pickle.load(f)
+with open("./scaler_y.pickle", mode='rb') as f:
+    scaler_y_standardization = pickle.load(f)
+
+np_x_col = scaler_X_standardization.transform(np.array(x_col))
+
+pre = regressor.predict(np_x_col)
+
+pred_result = scaler_y_standardization.inverse_transform(pre)
+print(pred_result)
